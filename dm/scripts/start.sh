09670f91b22d6acdb07b19814a816f5a4e18659c
GloryJeong <104194709+GloryJeong@users.noreply.github.com>
2022-09-01 15:50:12 +0900

    Update start.sh

--- dm/scripts/start.sh
+++ dm/scripts/start.sh
@@ -4,13 +4,13 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=freelec-springboot2-webservice
+REPOSITORY=/home/ubuntu/dm/step3
+PROJECT_NAME=dm
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -19,7 +19,7 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
